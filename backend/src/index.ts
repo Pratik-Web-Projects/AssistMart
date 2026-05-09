dd68203938ebafad3a9a38e2ed325d252ce070fe
Pratik Chavan <pratikchavan470@gmail.com>
2026-05-09 19:32:05 +0530

    Inital setup for assistMart

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhookHandler } from './webhooks/clerk';
+import { getEnv } from './lib/env';
+
+const env = getEnv();
+const app = express();
+
+const rawJson = express.raw({ type: "application/json", limit: "1mb" });
+
+app.post('/webhooks/clerk', rawJson, (req, res) => {
+    void clerkWebhookHandler(req, res);
+})
+
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+
+app.listen(env.PORT, ()=> console.log(`listen at ${env.PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import './App.css'
+
+function  App() {
+
+  return (
+    <>
+     <header>
+        <Show when="signed-out">
+          <SignInButton mode='modal'/>
+          <SignUpButton mode='modal'/>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </>
+  )
+}
+
+export default App
